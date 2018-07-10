8e9df45dbcb95ca246c8bd7559ee76b79afee7b1
clflovepanda <982294498@qq.com>
2018-07-10 11:30:37 +0800

    init

---
+++ src/main/java/com/duyi/oa/controller/IndexController.java
@@ -0,0 +1,32 @@
+package com.duyi.oa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by changlifeng on 16/11/25.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "test")
+    @ResponseBody
+    public Map<String, Object> test() {
+        System.out.println("lalalala");
+        Map<String, Object> result = new HashMap<>();
+        result.put("status", "ok");
+        return result;
+    }
+
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public ModelAndView index() {
+        return new ModelAndView("main");
+    }
+
+}

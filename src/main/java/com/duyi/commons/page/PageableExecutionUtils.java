a53d766f09e0e5c758afae113d677beb48f871b0
wangyan <kingyan09@qq.com>
2018-07-25 17:18:30 +0800

    Merge branch 'wy-dev'
    fixed bug

--- src/main/java/com/duyi/commons/page/PageableExecutionUtils.java
+++ src/main/java/com/duyi/commons/page/PageableExecutionUtils.java
@@ -15,7 +15,12 @@ public abstract class PageableExecutionUtils {
 	public PageableExecutionUtils() {
 		
 	}
-	
+	public static Pageable calculatePageable(int count, Pageable pageable) {
+		if(count<(pageable.getOffset()+pageable.getPageSize())) {
+			pageable=pageable.previousOrFirst();
+		}
+		return pageable;
+	}
 	/**
 	 * Constructs a {@link Page} based on the given {@code content}, {@link Pageable} and {@link TotalSupplier} applying
 	 * optimizations. The construction of {@link Page} omits a count query if the total can be determined based on the
